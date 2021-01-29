32627308830f7c03cb1d10767423df48efbb9f66
zhengkai <zhengkai@bignox.com>
2021-01-29 11:11:51 +0800

    go mod重构目录

---
+++ main/ngrok/ngrok.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/zk-123/ngrok/client"
+)
+
+func main() {
+	client.Main()
+}
---
+++ main/ngrokd/ngrokd.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/zk-123/ngrok/server"
+)
+
+func main() {
+	server.Main()
+}
